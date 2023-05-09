6bcc460ca53bdfe89d05f63a794fc48b610f3dd9
Naomi Gong <naomigong2005@gmail.com>
2023-05-08 19:31:22 -0700

    fixed too many bugs

--- outdoorpark.py
+++ outdoorpark.py
@@ -1,15 +1,22 @@
 #Naomi Gong
 #Outdoor Park 
+
+n_row = 20
+n_col = 26
+global num_tickets
+global seating
+
 def create_seating():
     """
     This example code creates a 2d list (2d matrix) that can store seating.
     The matrix is populated with . since all seats are available
     """
+    
     # our test matrix has 4 rows and 10 columns
-     # available seat
+     # available seatv
+    seating = []
     available_seat = '.'
     # create some available seating
-    seating = []
     for r in range(n_row):
         row = []
         for c in range(n_col):
@@ -17,10 +24,13 @@ def create_seating():
         seating.append(row)
     return seating 
 
+seating = create_seating()
+
 def print_seating(seat_data):
     let = "a"
     let_array = ["a"]
-    for letetr in range(25):
+    for letter in range(25):
+        #changes to unicode
         i = ord(let)
         i = i + 1
         let = chr(i)
@@ -35,15 +45,77 @@ def print_seating(seat_data):
         for c in range(n_col):
             print(seat_data[r][c], end=" ")
         print()
-    return None
+    return seat_data
 
-# our test matrix has 4 rows and 10 columns
-n_row = 20
-n_col = 26
+def update(new_seat, seat_num, num_tickets):
+    if start != 0:
+        i = 0
+    col = seat_num[len(seat_num) - 1]
+    row = int(seat_num[:-1])
+    col = ord(col) -97
+    n = 0
+    while n <= num_tickets + 2:
+        new_seat[row - 1][col + n] = "X"
+        new_seat[row + 1][col + n] = "X"
+        n = n + 1
+    print('c')
+    seating = new_seat
+    return None
+#declared dictionary for names and emails
+name_and_emailDict = {}
+
+def check_seats(num_tickets, seat_num):
+    open = True
+    row = int(seat_num[0])
+    col = seat_num[1]
+    col = ord(col) - 97
+
+    for c in range(num_tickets):
+        if col + c > 25:
+            return open
+        else:
+            if seating[row][col + c] != ".":
+                open = False
+
+    for c in range(num_tickets):
+        if open:
+            seating[row][col + c] = "e"
+    return open
+
+
+def buy_tickets():
+    num_tickets = int(input("How many tickets would you want to buy?"))
+    seat_num = input("Which seats would you like to buy ex. A0")
+    seats_avail = check_seats(num_tickets, seat_num)
+    if seats_avail:
+        update(seating, seat_num, num_tickets)
+    name = input("what is your name? ")
+    email = input("what is your email address?" )
+    seat_type = input("What type of seats would you like to buy?").lower()
+    name_and_emailDict[name] = seat_num
+    seatcost = 0
+    if seat_type == "front":
+        seatcost = 80
+    elif seat_type == "middle":
+        seatcost = 50
+    else:
+        seatcost = 25
+    total_cost = seatcost * int(num_tickets) * 0.0725
+    print(f"{'name: ': <15}", name)
+    print(f"{'email: ': <15}", email)
+    print(f"{'number of tickets: ': <15}", num_tickets)
+    print(f"{'total: ': <15}", total_cost)
+
+
+def search_buyer():
+    name = input("Please enter your name to see the tickets purchased")
+
 dash = "-" * 40
 print(dash)
 print("Outdoor Park")
 print(dash)
+start = 0
+
 
 userquit = False
 while (not userquit):
@@ -54,6 +126,13 @@ while (not userquit):
     command = input("Enter command: ")
 
     if command == "v":
-        new_seat = create_seating()
-        print_seating(new_seat)
+        if start == 0:
+            start_seat = create_seating()
+            new_seat = print_seating(start_seat)
+        else: 
+            new_seat = print_seating(start_seat)
+    elif command == "b":
+        buy_tickets()
+    elif command == "s":
+        search_buyer()
 
