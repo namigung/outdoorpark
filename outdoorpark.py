3b06fe5091bf869050cec4b683be0521e581b15a
Naomi Gong <naomigong2005@gmail.com>
2023-05-10 16:19:55 -0700

    finished!!

--- outdoorpark.py
+++ outdoorpark.py
@@ -1,6 +1,10 @@
 #Naomi Gong
 #Outdoor Park 
 
+import json
+from receipt import receipt
+
+
 n_row = 20
 n_col = 26
 global num_tickets
@@ -26,6 +30,7 @@ def create_seating():
 
 seating = create_seating()
 
+
 def print_seating(seat_data):
     let = "a"
     let_array = ["a"]
@@ -48,42 +53,54 @@ def print_seating(seat_data):
     return seat_data
 
 def update(new_seat, seat_num, num_tickets):
+
     col = seat_num[len(seat_num) - 1]
     row = int(seat_num[:-1])
+    print(row)
     col = ord(col) -97
+    print(col)
     n = 0
-    while n <= num_tickets + 1 and col + n < 27:
-        new_seat[row - 1][col - 1 + n] = "X"
-        new_seat[row + 1][col -  1 + n] = "X"
+
+    #prints X
+    for c in range(num_tickets):
+        if open:
+            new_seat[row][col + c] = "X"
+
+    #prints e in row above
+    while n <= num_tickets + 3 and col + n - 1 < 27:
+        new_seat[row - 1][col - 2 + n] = "e"
+        new_seat[row + 1][col -  2 + n] = "e"
+        if col - 2 + n < 0:
+            new_seat[row - 1][col - 2 + n] = "."
+            new_seat[row + 1][col -  2 + n] = "."
         n = n + 1
-    if col - 1 != 0:
-        new_seat[row][col - 1] = "X"
-    if col + num_tickets - 1 < 26:
-        new_seat[row][col + num_tickets] = "X"
-    print('c')
+    
+    if col - 2 >= 0:
+        n = 0
+        while n < 2:
+            new_seat[row][col - 1 - n] = "e"
+            n = n + 1
+    if col + num_tickets + 2 < 26:
+        n = 0
+        while n < 2:
+            new_seat[row][col + num_tickets + n] = "e"
+            n = n + 1
     seating = new_seat
     return None
 
-#declared dictionary for names and emails
-name_and_emailDict = {}
 
 def check_seats(num_tickets, seat_num):
     open = True
-    row = int(seat_num[0])
-    col = seat_num[1]
+    col = seat_num[len(seat_num) - 1]
+    row = int(seat_num[:-1])
     col = ord(col) - 97
 
     for c in range(num_tickets):
-        if col + c > 25:
-            return open
-        else:
-            if seating[row][col + c] != ".":
-                open = False
-
-    for c in range(num_tickets):
-        if open:
-            seating[row][col + c] = "e"
-    return open
+       if col + c > 26:
+           return False
+       if seating[row][col + c] != ".":
+           return False
+    return True
 
 
 def buy_tickets():
@@ -92,10 +109,21 @@ def buy_tickets():
     seats_avail = check_seats(num_tickets, seat_num)
     if seats_avail:
         update(seating, seat_num, num_tickets)
+    if seats_avail == False:
+        print("This seat is not available")
+        return None
     name = input("what is your name? ").lower()
     email = input("what is your email address?" )
-    seat_type = input("What type of seats would you like to buy?").lower()
-    name_and_emailDict[name] = str(num_tickets) + " starting at " + seat_num + ", " + seat_type
+
+    col = seat_num[len(seat_num) - 1]
+    row = int(seat_num[:-1])
+    
+    if row >= 1 and row <= 5:
+        seat_type = "front"
+    elif row >= 5 and row <= 10:
+        seat_type = "middle"
+    else:
+        seat_type = "back"
     seatcost = 0
     if seat_type == "front":
         seatcost = 80
@@ -104,21 +132,55 @@ def buy_tickets():
     else:
         seatcost = 25
     total_cost = seatcost * int(num_tickets) * 1.0725
+     
+    p = receipt(name, email, num_tickets, seat_type, seat_num, total_cost)
+
+    purchase.append(p)
+    #change code here
+    
     print(f"{'name: ': <15}", name)
     print(f"{'email: ': <15}", email)
     print(f"{'number of tickets: ': <15}", num_tickets)
     print(f"{'total: ': <15}", total_cost)
 
 
+def update_json():
+    recieptList = [p.__dict__ for p in purchase]
+    with open ('purchase.json', 'w') as json_file:
+        json.dump(recieptList, json_file)
+    json_file.close()
+    
+
+
+
 def search_buyer():
     name = input("Please enter your name to see the tickets purchased")
-    if name in name_and_emailDict:
-        print(name +  ", you purchased: " + name_and_emailDict[name])
+    for person in purchase:
+        if person.name == name:
+            person.print_receipt()
+            return None
+    print("Person not found")
 
-    else:
-        print(name + "did not purchase any ticket")
+def display_All():
+    for person in purchase:
+        person.print_receipt()
+    total = 0 
+    for person in purchase:
+        total = total + person.cost
+    dash = "-" * 40
+    print(dash)
+    print("The total income is " + str(total))
 
-        
+purchase = []
+try: 
+    with open('purchase.json', 'r') as json_file:
+        purchase_data = json.load(json_file)
+    purchase = [receipt(p['name'], p['email'], p['num_tickets'], p['seat_type'], p['seat_num'], p['cost']) for p in purchase_data]
+except (Exception):
+    pass
+
+for p in purchase:
+    update(seating, p.seat_num, p.num_tickets)
 
 dash = "-" * 40
 print(dash)
@@ -141,4 +203,11 @@ while (not userquit):
         buy_tickets()
     elif command == "s":
         search_buyer()
+    elif command == "d":
+        display_All()
+    elif command == 'q':
+        update_json()
+        userquit = True
+    else:
+        print("command not found")
 
---
+++ receipt.py
@@ -0,0 +1,37 @@
+import json
+
+class receipt:
+
+    def __init__(self, name, email, num_tickets, seat_type, seat_num, cost):
+        self.name = name
+        self.email = email
+        self.num_tickets = num_tickets
+        self.seat_type = seat_type
+        self.seat_num = seat_num
+        self.cost = cost
+
+    def list_to_dict(self):
+        return 
+
+    def print_receipt(self):
+        dash = '-' * 40
+        print(dash)
+        print(f"{'name': <20}", end = "")
+        print(f"{self.name: >20}")
+        print(f"{'email': <20}", end = "")
+        print(f"{self.email: >20}")
+        print(f"{'number of tickets purchased': <20}", end = "")
+        print(f"{self.num_tickets: >15}")
+        print(f"{'type of seats': <20}", end = "")
+        print(f"{self.seat_type: >20}")
+        print(f"{'seat number (start)': <20}", end = "")
+        print(f"{self.seat_num: >20}")
+        print(f"{'total cost': <20}", end = "")
+        print(f"{self.cost: >20}")
+        dash = '-' * 40
+        
+        
+
+        
+    
+    
